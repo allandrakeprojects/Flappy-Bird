5ee841d5371917233d664c27fe421faa9474463f
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2019-07-04 13:11:10 +0800

    Add

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameHandler : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+        Score.Start();
+    }
+
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using CodeMonkey.Utils;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    private Text scoreText;
+
+    private void Awake()
+    {
+        scoreText = transform.Find("scoreText").GetComponent<Text>();
+        transform.Find("retryButton").GetComponent<Button_UI>().ClickFunc = () => { UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene"); };
+    }
+
+    private void Start()
+    {
+        Bird.GetInstance().OnDied += Bird_OnDied;
+        Hide();
+    }
+
+    private void Bird_OnDied(object sender, EventArgs e)
+    {
+        scoreText.text = Level.GetInstance().GetPipesPassedCount().ToString();
+        Show();
+    }
+
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
